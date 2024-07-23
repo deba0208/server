0c589dc8f68d88c9b532c3a0c29625bf7985bcc7
Debashis Gayen <debashisgayen02@gmail.com>
2024-07-23 23:33:51 +0530

    .

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["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"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
